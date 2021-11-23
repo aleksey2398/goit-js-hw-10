d64ff3bb5ea5af04a6fa69f16f25780851373398
aleksey2398 <87311512+aleksey2398@users.noreply.github.com>
2021-11-23 15:06:47 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,88 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix, { Notify } from 'notiflix';
+// import countryCardTemplate from './country-card.hbs';
+// import countryListTemplate from './country-list.hbs';
+import { fetchCountries } from'./fetchCountries';
 
+const countryInput = document.querySelector('#search-box');
+const countryCardsWrapperEl = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
+const cleanEl = el => (el.innerHTML = '');
 const DEBOUNCE_DELAY = 300;
+
+const countryCheck = e => {
+  const inputValue = e.target.value.trim();
+  if (!inputValue) {
+    cleanEl(countryList);
+    cleanEl(countryCardsWrapperEl);
+    return;
+  }
+  fetchCountries(inputValue)
+    .then(countryData => {
+      if (countryData.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+        return;
+      }
+      searchCountry(countryData);
+    })
+    .catch(error => {
+      cleanEl(countryList);
+      cleanEl(countryCardsWrapperEl);
+      Notify.failure('Oops, there is no country with that name');
+    });
+};
+
+const renderCountryCard = countryData => {
+  if (countryData.length === 1) {
+    cleanEl(countryList);
+    const markupOneCountry = createMarkupOneCountry(countryData);
+    countryCardsWrapperEl.innerHTML = markupOneCountry;
+  } else if (countryData.length <= 10) {
+    cleanEl(countryCardsWrapperEl);
+    const markupManyCountry = createMarkupManyCountry(countryData);
+    countryList.innerHTML = markupManyCountry;
+  }
+};
+
+const createMarkupManyCountry = countryData => {
+  return countryData
+    .map(({ name, flags }) => `<div class="list-container"><img class="list-flag" src="${flags.png}" alt="${name.official}" width="60px" height="40px"></img>
+<h2 class="country-list-name">${name.official}</h2></div>`)
+    .join('');
+};
+
+const createMarkupOneCountry = countryData => {
+  return countryData
+    .map(({ name, flags, capital, population, languages }) => `<div class="country__card">
+  <img class="country-flag" src="${flags.png}" alt="${name.official}" width="300px" height="200px"></img>
+  <h2 class="country-name">${name.official}</h2>
+  <ul class="country-info">
+  <li class="country-info-item">
+      <p class="country-capital">Capital: <span class="country-elem">${capital}</span></p>
+    </li>
+  <li class="country-info-item">
+      <p class="country-population">Population: <span class="country-elem">${population}</span></p>
+    </li>
+  <li class="country-info-item">
+      <p class="country-languages">Languages: <span class="country-elem">${Object.values(languages)}</span></p>
+    </li>
+  </ul>
+</div>`)
+}
+
+const searchCountry = (event) => {
+  const country = countryInput.value;
+
+  fetchCountries(country)
+    .then(countryData => {
+    console.log(countryData);
+    renderCountryCard(countryData);
+  })
+  .catch(error => {
+    console.log(error);
+    Notify.failure("Oops, there is no country with that name");
+  });
+};
+
+countryInput.addEventListener('input', debounce(countryCheck, DEBOUNCE_DELAY));
